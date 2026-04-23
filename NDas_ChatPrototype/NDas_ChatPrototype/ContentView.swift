e7c8cebb0c4c450a9755633fd0e464b169e5980f
Nillo <ndas29@student.kis.or.kr>
2026-04-23 09:14:36 +0900

    Create XCode ChatPrototype Project

---
+++ NDas_ChatPrototype/NDas_ChatPrototype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NDas_ChatPrototype
+//
+//  Created by Nillohit Das on 4/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NDas_ChatPrototype/NDas_ChatPrototype/NDas_ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NDas_ChatPrototypeApp.swift
+//  NDas_ChatPrototype
+//
+//  Created by Nillohit Das on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct NDas_ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
